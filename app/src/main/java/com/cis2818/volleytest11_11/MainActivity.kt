bc43513a6e481eaa85b4bfeaa01f8f07c30391c9
Tom <tomadesrosiers@gmail.com>
2024-11-19 12:33:54 -0500

    Test

--- app/src/main/java/com/cis2818/volleytest11_11/MainActivity.kt
+++ app/src/main/java/com/cis2818/volleytest11_11/MainActivity.kt
@@ -28,7 +28,12 @@ class MainActivity : AppCompatActivity() {
 
         //set button handler
         binding.btnGetCatData.setOnClickListener {
-            printCatData()
+          //  printCatData()
+
+            printCoinAPI()
+
+            Log.i("MainActivity","After Click Event")
+
         } //call this other function }
 
     }//end fun OnCreate
@@ -62,5 +67,47 @@ class MainActivity : AppCompatActivity() {
                     // Add the request to the RequestQueue.
                      queue.add(stringRequest)
             }//end printCatData
+
+    // method to interact with API for COIN API
+    private fun printCoinAPI() {
+
+        var coinUrl = "https://api.coincap.io/v2/assets"
+
+        val queue = Volley.newRequestQueue(this)
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, coinUrl,
+            Response.Listener<String> { response ->
+                var coinArray : JSONArray = JSONArray(response)
+
+
+                //indices from 0 through catsArray.length()-1
+/*
+
+                for(i in 0 until coinArray.length()) {
+                    //${} is to interpolate the string /
+                    // uses a string template
+
+                    Log.i("MainActivity", "In for loop")
+                  //  var theCoin : JSONObject = coinArray.getJSONObject(i)
+                    //now get the properties we want: name and description
+                 //   Log.i("MainActivity", "Cat name: ${theCoin.getString("name")}")
+                  //  Log.i("MainActivity", "Cat description: ${theCoin.getString("symbol")}")
+                }//end for
+*/
+
+            },
+            Response.ErrorListener {
+                Log.i("MainActivity", "That didn't work!")
+            })
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+    }//end printCoinAPI
+
+
+
+
+
 
 } //End MainActivity
