19508b61e2999d784d8f9ba87e1d1f093a64e5fb
Tom.DesRosiers <tomadesrosiers@gmail.com>
2024-11-22 22:20:08 -0500

    AS

--- app/src/main/java/com/cis2818/volleytest11_11/MainActivity.kt
+++ app/src/main/java/com/cis2818/volleytest11_11/MainActivity.kt
@@ -1,51 +1,35 @@
 package com.cis2818.volleytest11_11
 
-
+//import android.R
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import androidx.activity.enableEdgeToEdge
-import androidx.annotation.IntegerRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
-
-import com.cis2818.volleytest11_11.databinding.ActivityMainBinding
-import org.json.JSONArray
-import org.json.JSONObject
-import org.json.JSONException
-
 import com.android.volley.Request
-import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.android.volley.toolbox.StringRequest
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeUnit.SECONDS
+import com.cis2818.volleytest11_11.databinding.ActivityMainBinding
+import org.json.JSONArray
 
 
-class MainActivity : AppCompatActivity() {
+/** CLASS: MAIN ACTIVITY **/
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    //Class Data
+    //Data
     private lateinit var binding : ActivityMainBinding
     private lateinit var viewModel: CoinViewModel
-    private lateinit var rawData : JSONArray
-    private lateinit var test : String
+    private var dropDownChoices = arrayOf<String>()
 
     /**
-     * Function: onCreate
+     * FUNCTION: onCreate
      */
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
 
         super.onCreate(savedInstanceState)
 
@@ -56,47 +40,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Get the coin data from API and save to the viewModel
-        //Populate the spinner dropdown
-        //Populate initial record
+        /**
+         * Get the coin data from API and save to the viewModel
+         * Populate the spinner dropdown
+         * Populate initial record
+        **/
          fetchAndSetCoinData()
 
-
-
-
-
-        var languages = arrayOf("Java", "PHP", "Kotlin", "Javascript", "Python", "Swift")
-
-        val spinner: Spinner = findViewById(R.id.spCoinChoice)
-
-       // binding.spCoinChoice. --> View Binding?
-
-        // Create an ArrayAdapter using the string array and a default spinner layout.
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.planets_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears.
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner.
-            spinner.adapter = adapter
-        }
-
-/*
-        binding.spCoinChoice.setOnClickListener{
-           Log.i("MainActivity","Spinner Clicked")
-        }
-*/
-
-
-    }//end fun OnCreate
-
+    }//end function -  OnCreate
 
     /**
      * FUNCTION: fetchCoinData
      */
-    private fun fetchAndSetCoinData()  {
+    private fun fetchAndSetCoinData()
+    {
 
         //RequestQueue object
         val requestQueue = Volley.newRequestQueue(this)
@@ -107,44 +64,118 @@ class MainActivity : AppCompatActivity() {
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
             Response.Listener { response ->
+
                 //Create a JSON Array from the JSON object returned
                   var dataArray : JSONArray = response.getJSONArray("data")
 
-                   viewModel.setCoinData(dataArray)
+                //Set ViewModel
+                viewModel.setCoinData(dataArray)
 
+                //Populate dropdown choice Array
+                populateDropDownChoiceArray()
 
-                   var currObject = viewModel.getACurrency(2)
+                //Set Up Spinner
+                setUpSpinner()
 
-                   Log.i("MainActivity","Result: ${currObject.currName}")
-                   Log.i("MainActivity","Result: ${currObject.supply}")
-
-                    Log.i("MainActivity","Length: ${viewModel.getLength()}")
-
+                //Populate Initial View
+                setInitialView()
 
                  },
             Response.ErrorListener { error ->
                 Log.i("MainActivity", "That didn't work!")
-
             }
         )//end call to API
 
         //Add to requestQueue
         requestQueue.add(jsonObjectRequest)
 
-
-    } //end fetchCoinData
-
-/*
-    //Get A capital
-    binding.capitalInfo.setText(viewModel.getCapital())
+    } //end function - fetchCoinData
 
     /**
-     * OnClickListener: Function
+     * FUNCTION: onItemSelected
      */
-    binding.nextButton.setOnClickListener {
-        //Get a capital
-        binding.capitalInfo.setText(viewModel.getCapital())
-    }//end setOnClickListener
-*/
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
+    {
+        val intPosition = parent?.getItemIdAtPosition(position)
 
-} //End MainActivity
+        if (intPosition != null) {
+            updateView(intPosition)
+        }
+    }// end function - onItemSelected
+
+    /**
+     * FUNCTION: onNothingSelected
+     */
+    override fun onNothingSelected(parent: AdapterView<*>?)
+    {
+        Log.i("Main Activity", "On Nothing Item Selected Clicked")
+    }//end function - onNothingSelected
+
+    /**
+     *  FUNCTION: populateDropDownChoiceArray - Populate Dropdown Choice Array
+     */
+    private fun populateDropDownChoiceArray()
+    {
+        //Get Length
+         val lengthItems = viewModel.getLength()
+         val namesList = dropDownChoices.toMutableList()
+        //Loop thru each one
+        for(i in 0 until lengthItems)
+        {
+            val currObject = viewModel.getACurrency(i)
+
+           // Log.i("MainActivity","Name: ${currObject.currName} and ${i} ")
+            namesList.add(currObject.currName)
+        }
+        dropDownChoices = namesList.toTypedArray()
+    }//end function - populateDropdownChoices
+
+   /**
+    * FUNCTION: setUpSpinner - Bind Spinner dropdown
+    */
+   private fun setUpSpinner()
+   {
+       //Bind Spinner object
+       val spinner: Spinner = findViewById(R.id.spCoinChoice)
+
+       val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(
+           this,
+           androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,  dropDownChoices
+       )
+       spinner.setAdapter(arrayAdapter)
+       spinner.onItemSelectedListener = this
+    }//end function - setUpSpinner
+
+   /**
+    * FUNCTION: setInitialView
+    */
+   private fun setInitialView()
+   {
+      //Get first record
+       val currObject = viewModel.getACurrency(0)
+       //Get A capital
+       binding.txtName.text = currObject.currName
+       binding.txtSymbol.text = currObject.symbol
+       binding.txtSupply.text = currObject.supply
+       binding.txtPrice.text = currObject.price
+       binding.txtPercChange.text = currObject.percChange
+    }//end function - populateInitialView
+
+  /** FUNCTION: updateView **/
+  private fun updateView(itemNumber: Long)
+  {
+      val intValue: Int = itemNumber.toInt()
+      //Get first record
+      val currObject = viewModel.getACurrency(intValue)
+
+      binding.txtName.text = currObject.currName
+      binding.txtSymbol.text = currObject.symbol
+      binding.txtSupply.text = currObject.supply
+      binding.txtPrice.text = currObject.price
+      binding.txtPercChange.text = currObject.percChange
+
+  }//end function - updateView
+
+}//end CLASS: MainActivity
+
+
